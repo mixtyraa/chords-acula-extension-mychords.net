ac6e88fd2df306b7c2b660fc27693121067bf79b
mixtyraa <mixtyraa@mail.ru>
2019-04-27 15:02:07 +0500

    update gitignore

--- chords-acula-extension/background.js
+++
@@ -1,5 +0,0 @@
-const urlSrtiptAcula = chrome.runtime.getURL("chords-acula-main.js");
-const scriptAcula = document.createElement("script");
-scriptAcula.setAttribute("type", "text/javascript");
-scriptAcula.src = urlSrtiptAcula;
-document.getElementsByTagName("head")[0].appendChild(scriptAcula);
