1c84983a8574581d0c69f05081d90fae2d6090a6
mixtyraa <mixtyraa@mail.ru>
2019-04-27 14:41:08 +0500

    Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,61 @@
-const { src, dest, parallel } = require('gulp');
+const {
+  src,
+  dest,
+  parallel,
+  watch,
+  series
+} = require('gulp');
 const babel = require('gulp-babel');
 const minify = require("gulp-babel-minify");
+const flatten = require('gulp-flatten');
+const removeLogging = require("gulp-remove-logging");
 
-function js() {
+const buildPath = 'chords-acula-extension';
+
+function buildJs() {
   return src('src/**/*.js')
-  .pipe(babel({
+    .pipe(removeLogging({
+      namespace: ['console', 'window.console']
+    }))
+    .pipe(babel({
       presets: ['@babel/env']
-  }))
-  .pipe(minify({
-    mangle: {
-      keepClassName: true
-    }
-  }))
-  .pipe(dest('chords-acula-extension'))
+    }))
+    .pipe(minify({
+      mangle: {
+        keepClassName: true
+      }
+    }))
+    .pipe(flatten())
+    
+    .pipe(dest(buildPath))
 }
 
-exports.js = js;
-exports.build = js;
+function icons() {
+  return src('src/icons/*.png')
+    .pipe(dest(buildPath))
+}
+
+function devJs() {
+  return src('src/**/*.js')
+  .pipe(flatten())
+  .pipe(dest(buildPath))
+
+}
+
+function dev(cb) {
+
+  watch(['src/**/*.js'], parallel(devJs));
+  watch(['src/icons/*.js'], parallel(icons));
+
+  cb();
+}
+
+function build(cb) {
+  buildJs();
+  icons();
+  cb();
+}
+
+exports.build = build;
+exports.dev = dev;
 exports.default = exports.build;
